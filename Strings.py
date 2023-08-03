656de9bf3b0254fd31f207e7cae3b93a533e6fb4
gusta <gustavoandres_r@hotmail.com>
2023-08-03 18:44:34 +0200

    Añadido de cadenas con variables y textos multilínea

---
+++ Strings.py
@@ -0,0 +1,14 @@
+""" Strings """
+
+"Agregando cadenas a través de Variables"
+
+fact = "The Moon has no atmosphere."
+two_facts = fact + " No sound can be heard on the Moon."
+print(two_facts)
+
+" Se pueden usar comillas simples, dobles o triples para las cadenas"
+
+" Texto Multilinea. Lo usamos para separar en varias lineas el texto "
+
+multiline = "Facts about the Moon:\n There is no atmosphere.\n There is no sound."
+print(multiline)
