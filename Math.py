65b7e3b762aec4490c70d1886b59e242cd374cd4
gusta <gustavoandres_r@hotmail.com>
2023-08-10 17:51:12 +0200

    Operaciones matemáticas

---
+++ Math.py
@@ -0,0 +1,38 @@
+"Operadores Matématicos"
+
+"Suma"
+
+answer = 30 + 12
+print(answer)
+
+"Resta"
+
+difference = 30 - 12
+print(difference)
+
+"Multiplicación"
+
+product = 30 * 12
+print(product)
+
+"División"
+
+quotient = 30 / 12
+print(quotient)
+
+"convertir un número de segundos en minutos y segundos para su visualización"
+
+# Minutos
+seconds = 1042
+display_minutes = 1042 // 60 # // Redondeo hacia abajo (división de múltiplo inferior)
+print(display_minutes)
+
+# Segundos
+
+seconds = 1042
+display_minutes = 1042 // 60
+display_seconds = 1042 % 60
+
+print(display_minutes)
+print(display_seconds)
+
