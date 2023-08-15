44898ff86a0858800a82df40794a952e63bb7d67
gusta <gustavoandres_r@hotmail.com>
2023-08-15 13:03:47 +0200

    Usos del diccionario

--- dictionaries.py
+++ dictionaries.py
@@ -33,3 +33,62 @@ planet['name'] = 'Jupiter'
 planet['moons'] = 79
 
 # No output: name is now set to Makemake.
+
+"Adición y eliminación de claves"
+
+planet['orbital period'] = 4333
+
+# planet dictionary now contains: {
+#   name: 'jupiter'
+#   moons: 79
+#   orbital period: 4333
+# }
+
+planet.pop('orbital period')
+
+# planet dictionary now contains: {
+#   name: 'jupiter'
+#   moons: 79
+# }
+
+"Tipos de data complejos"
+
+"""Los diccionarios pueden almacenar cualquier tipo de valor, 
+incluidos otros diccionarios. Esto le permite modelar datos complejos 
+según sea necesario."""
+
+# Add address
+planet['diameter (km)'] = {
+    'polar': 133709,
+    'equatorial': 142984
+}
+
+# planet dictionary now contains: {
+#   name: 'Jupiter'
+#   moons: 79
+#   diameter (km): {
+#      polar: 133709
+#      equatorial: 142984
+#   }
+# }
+
+print(f'{planet["name"]} polar diameter: {planet["diameter (km)"]["polar"]}')
+
+"Ejercicio"
+
+planet = {
+    'name' : "Mars",
+    'moons' : 2
+}
+
+print(f'{planet["name"]} has {planet["moons"]} moon(s)')
+
+planet.update({
+    'circumference (km)': {
+        'polar': 6752,
+        'equatorial': 6792
+    }
+})
+
+print(f'{planet["name"]} has a polar circumference of {planet["circumference (km)"]["polar"]}')
+
