071d4de9cd30da2bc21080730cc921b54809fa38
gusta <gustavoandres_r@hotmail.com>
2023-08-15 14:20:17 +0200

    funciones y usos

--- 09_functions.py
+++ 09_functions.py
@@ -54,3 +54,74 @@ def generate_report(main_tank, external_tank, hydrogen_tank):
     Hydrogen tank: {hydrogen_tank} 
     """
 print(generate_report(100, 200, 300))
+
+"argumentos de palabra clave"
+
+from datetime import timedelta, datetime
+
+def arrival_time(hours=51):
+    now = datetime.now()
+    arrival = now + timedelta(hours=hours)
+    return arrival.strftime("Arrival: %A %H:%M")
+
+print(arrival_time())
+
+print(arrival_time(hours=0))
+
+"Combinación de argumentos y argumentos de palabra clave"
+
+from datetime import timedelta, datetime
+
+def arrival_time(destination, hours=51):
+    now = datetime.now()
+    arrival = now + timedelta(hours=hours)
+    return arrival.strftime(f"{destination} Arrival: %A %H:%M")
+
+print(arrival_time("Moon"))
+print(arrival_time("Orbit", hours=0.13))
+
+"Argumentos de variable"
+
+def variable_length(*args):
+    print(args)
+
+variable_length()
+()
+variable_length("one", "two")
+('one', 'two')
+variable_length(None)
+(None,)
+
+def sequence_time(*args):
+    total_minutes = sum(args)
+    if total_minutes < 60:
+        return f"Total time to launch is {total_minutes} minutes"
+    else:
+        return f"Total time to launch is {total_minutes/60} hours"
+
+print(sequence_time(4, 14, 18))
+
+"Argumentos de palabra clave variable"
+
+def variable_length(**kwargs):
+    print(kwargs)
+
+variable_length(tanks=1, day="Wednesday", pilots=3)
+{'tanks': 1, 'day': 'Wednesday', 'pilots': 3}
+
+"Ejemplo"
+
+def crew_members(**kwargs):
+    print(f"{len(kwargs)} astronauts assigned for this mission:")
+    for title, name in kwargs.items():
+        print(f"{title}: {name}")
+
+crew_members(captain="Neil Armstrong", pilot="Buzz Aldrin", command_pilot="Michael Collins")
+
+"Ejercicio"
+
+def fuel_report(**fuel_tanks):
+    for name, value in fuel_tanks.items():
+        print(f'{name}: {value}')
+
+fuel_report(main=50, external=100, emergency=60)
