af469135daccfaedb75482c2c9b4a64cd6ef2500
gusta <gustavoandres_r@hotmail.com>
2023-08-15 13:32:41 +0200

    Diccionarios, usos y complementos

--- dictionaries.py
+++ dictionaries.py
@@ -118,3 +118,43 @@ else:
     rainfall['december'] = 1
 
 # Because december exists, the value will be 3.1
+
+"Recuperación de todos los valores"
+
+total_rainfall = 0
+for value in rainfall.values():
+    total_rainfall = total_rainfall + value
+
+print(f'There was {total_rainfall}cm in the last quarter.')
+
+"Ejercicio"
+
+planet_moons = {
+    'mercury' : 0,
+    'venus' : 0,
+    'earth' : 1,
+    'mars' : 2,
+    'jupiter' : 79,
+    'saturn' : 82,
+    'uranus' : 27,
+    'neptune' : 14,
+    'pluto' : 5,
+    'haumea' : 2,
+    'makemake' : 1,
+    'eris' : 1
+}
+
+"""recupere una lista con el número de lunas y guárdela en una variable llamada lunas. 
+Luego obtenga el número total de planetas y almacene ese valor 
+en una variable llamada total_planets."""
+
+moons = planet_moons.values()
+total_planets = len(planet_moons.keys())
+
+total_moons = 0
+for moon in moons:
+    total_moons = total_moons + moon
+
+average = total_moons / total_planets
+
+print(f'Each planet has an average of {average} moons')
