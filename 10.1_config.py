904088842148f4fbe0e6de79515ba2c5db2293f7
gusta <gustavoandres_r@hotmail.com>
2023-08-15 18:00:19 +0200

    Control de errores.

---
+++ 10.1_config.py
@@ -0,0 +1,83 @@
+"""def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")"""
+
+
+"""def main():
+    try:
+        configuration = open('config.txt')
+    except Exception:
+        print("Couldn't find the config.txt file!")"""
+
+"""def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+    except (BlockingIOError, TimeoutError):
+        print("Filesystem under heavy load, can't complete reading configuration file")"""
+
+loaded_config = """# Rocket Ship Configuration File!
+fuel_tanks=4
+oxygen_tanks=3
+initial_propulsion_level=84
+$ End of file
+
+parsed_config = {}
+for line in loaded_config.split('\n'):
+    try:
+        key, value = line.split('=')
+        parsed_config[key] = value
+    except ValueError:
+        print(f'Unable to parse {line}')
+print(parsed_config)"""
+
+"Generación de excepciones"
+
+"""def water_left(astronauts, water_left, days_left):
+    daily_usage = astronauts * 11
+    total_usage = daily_usage * days_left
+    total_water_left = water_left - total_usage
+    if total_water_left < 0:
+        raise RuntimeError(f"There is not enough water for {astronauts} astronauts after {days_left} days!")
+    return f"Total water left after {days_left} days is: {total_water_left} liters"
+
+
+print(water_left(5, 100, 2))
+
+try:
+    water_left(5, 100, 2)
+except RuntimeError as err:
+    alert_navigation_system(err)
+
+water_left("3", "200", None)"""
+
+def water_left(astronauts, water_left, days_left):
+    for argument in [astronauts, water_left, days_left]:
+        try:
+            # If argument is an int, the following operation will work
+            argument / 10
+        except TypeError:
+            # TypeError will be raised only if it isn't the right type 
+            # Raise the same exception but with a better error message
+            raise TypeError(f"All arguments must be of type int, but received: '{argument}'")
+    daily_usage = astronauts * 11
+    total_usage = daily_usage * days_left
+    total_water_left = water_left - total_usage
+    if total_water_left < 0:
+        raise RuntimeError(f"There is not enough water for {astronauts} astronauts after {days_left} days!")
+    return f"Total water left after {days_left} days is: {total_water_left} liters"
+
+water_left("3", "200", None)
--- 10_error_control.py
+++ 10_error_control.py
@@ -1,7 +1,22 @@
 "Seguimientos"
 
-def main():
+"""def main():
     open("/path/to/mars.jpg")
 
 if __name__ == '__main__':
-    main()
+    main()"""
+
+"""Los seguimientos casi siempre incluyen la información siguiente:
+
+Todas las rutas de acceso de archivo implicadas, para cada llamada a cada función.
+Los números de línea asociados a cada ruta de acceso de archivo.
+Los nombres de las funciones, métodos o clases implicados en la generación de una excepción.
+El nombre de la excepción que se ha producido.
+"""
+
+"Prueba y excepción de los bloques"
+
+try:
+     open('config.txt')
+except FileNotFoundError:
+     print("Couldn't find the config.txt file!")
